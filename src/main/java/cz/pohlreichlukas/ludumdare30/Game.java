8c1bfc2016e995fc832de0b110d0808bf816dc2d
Lukáš Pohlreich <pohlreichlukas@gmail.com>
2014-08-23 07:01:39 +0200

    Gradle script and basic window

---
+++ src/main/java/cz/pohlreichlukas/ludumdare30/Game.java
@@ -0,0 +1,12 @@
+package cz.pohlreichlukas.ludumdare30;
+
+import javax.swing.JFrame;
+
+public class Game {
+    
+    public static void main( String[] args ) {
+        JFrame frame = new JFrame( "Ludum Dare 30" );
+        frame.setVisible( true );
+        frame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+    }
+}

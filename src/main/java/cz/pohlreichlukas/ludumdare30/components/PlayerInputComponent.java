9e72deabe8b6b769cf0bbe1673c15eeea4e56edc
Lukas Pohlreich <pohlreichlukas@gmail.com>
2014-10-02 13:58:51 +0200

    Player entity cannot leave window

--- src/main/java/cz/pohlreichlukas/ludumdare30/components/PlayerInputComponent.java
+++ src/main/java/cz/pohlreichlukas/ludumdare30/components/PlayerInputComponent.java
@@ -22,8 +22,14 @@ public class PlayerInputComponent implements InputComponent<Player> {
     public void update( Player e, World world, GamePane pane, long delta ) {
         Point mousePosition = pane.getMousePosition();
         if ( mousePosition !=  null ) {
-            e.setX( (float) mousePosition.getX() - e.getWidth() / 2 );
-            e.setY( (float) mousePosition.getY() - e.getHeight() / 2 );
+            float newPosX = (float) mousePosition.getX() - e.getWidth() / 2;
+            float newPosY = (float) mousePosition.getY() - e.getHeight() / 2;
+            if ( newPosX < 0 ) newPosX = 0;
+            if ( newPosX + e.getWidth() > pane.getWidth() ) newPosX = pane.getWidth() - e.getWidth();
+            if ( newPosY < 0 ) newPosY = 0;
+            if ( newPosY + e.getHeight() > pane.getHeight() ) newPosY = pane.getHeight() - e.getHeight();
+            e.setX( newPosX );
+            e.setY( newPosY );
         }
 
         InputListener input = pane.getInput();
